c0ab77b00ab43b9c43785e1b61f5eba5a7bd573a
alPumpur <aleftinapumpur@gmail.com>
2024-02-16 22:55:21 +0700

    функционал

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,37 +1,3 @@
-// main.js
-Vue.component('kanban-column', {
-    props: ['id', 'title', 'tasks', 'allowEdit', 'allowMove'],
-    template: `
-        <div class="column" :id="id">
-            <h2>{{ title }}</h2>
-            <div v-for="(task, index) in tasks" :key="task.created">
-                <div class="task-card">
-                    <h3>{{ task.title }}</h3>
-                    <p>{{ task.description }}</p>
-                    <p>Дата создания: {{ task.created }}</p>
-                    <p>Дедлайн: {{ task.deadline }}</p>
-                    <p v-if="allowEdit">Последнее редактирование: {{ task.lastEdited }}</p>
-                    <button v-if="allowEdit" @click="editTask(task)">Редактировать</button>
-                    <button @click="deleteTask(index)">Удалить</button>
-                    <button v-if="allowMove" @click="moveTask(task)">В следующий столбец</button>
-                </div>
-            </div>
-        </div>
-    `,
-    methods: {
-        editTask(task) {
-            task.lastEdited = new Date().toLocaleString();
-            this.$emit('edit-task', task);
-        },
-        deleteTask(index) {
-            this.$emit('delete-task', index);
-        },
-        moveTask(task) {
-            this.$emit('move-task', task);
-        }
-    }
-});
-
 new Vue({
     el: '#app',
     data: {
@@ -41,12 +7,51 @@ new Vue({
         completedTasks: [],
         newTaskTitle: '',
         newTaskDescription: '',
-        newTaskDeadline: ''
+        newTaskDeadline: '',
+        editingTask: null, // Индекс задачи, которую мы редактируем
+        editedTask: { title: '', description: '', deadline: '' } // Объект для временного хранения отредактированных данных
     },
     methods: {
+        // Метод для открытия формы редактирования задачи
+        editTaskForm(index) {
+            this.editingTask = index;
+            // Заполнение формы текущими данными задачи
+            const task = this.plannedTasks[index] && this.inProgressTasks[index] && this.testingTasks[index];
+            this.editedTask = {
+                title: task.title,
+                description: task.description,
+                deadline: task.deadline
+            };
+        },
+        // Метод для сохранения отредактированной задачи
+        saveEditedTask() {
+            let task;
+            if (this.editingTask !== null) {
+                if (this.editingTask < this.plannedTasks.length) {
+                    task = this.plannedTasks[this.editingTask];
+                } else if (this.editingTask < this.plannedTasks.length + this.inProgressTasks.length) {
+                    task = this.inProgressTasks[this.editingTask - this.plannedTasks.length];
+                } else if (this.editingTask < this.plannedTasks.length + this.inProgressTasks.length + this.testingTasks.length) {
+                    task = this.testingTasks[this.editingTask - this.plannedTasks.length - this.inProgressTasks.length];
+                } else {
+                    task = this.completedTasks[this.editingTask - this.plannedTasks.length - this.inProgressTasks.length - this.testingTasks.length];
+                }
+
+                task.title = this.editedTask.title;
+                task.description = this.editedTask.description;
+                task.deadline = this.editedTask.deadline;
+                task.lastEdited = new Date().toLocaleString();
+            }
+            this.editingTask = null; // Закрыть форму редактирования
+        },
+
+        // Метод для отмены редактирования задачи
+        cancelEditTask() {
+            this.editingTask = null; // Закрыть форму редактирования
+        },
+        // Остальные методы добавления, удаления и перемещения задач остаются без изменений
         addTask(column) {
             if (this.newTaskTitle.trim() === '') return;
-
             const task = {
                 title: this.newTaskTitle,
                 description: this.newTaskDescription,
@@ -54,25 +59,54 @@ new Vue({
                 deadline: this.newTaskDeadline,
                 lastEdited: new Date().toLocaleString()
             };
-
             if (column === 'planned') {
                 this.plannedTasks.push(task);
             }
             this.clearForm();
         },
+        deleteTask(column, index) {
+            // Удаление задачи из соответствующего массива
+            if (column === 'planned') {
+                this.plannedTasks.splice(index, 1);
+            } else if (column === 'inProgress') {
+                this.inProgressTasks.splice(index, 1);
+            } else if (column === 'testing') {
+                this.testingTasks.splice(index, 1);
+            } else if (column === 'completed') {
+                this.completedTasks.splice(index, 1);
+            }
+        },
+        moveTask(task) {
+            const index = this.plannedTasks.indexOf(task);
+            if (index !== -1) {
+                this.plannedTasks.splice(index, 1);
+                this.inProgressTasks.push(task);
+                return; // Вернуться после перемещения в столбец "В работе"
+            }
+
+            const index1 = this.inProgressTasks.indexOf(task);
+            if (index1 !== -1) {
+                this.inProgressTasks.splice(index1, 1);
+                this.testingTasks.push(task);
+                return; // Вернуться после перемещения в столбец "Тестирование"
+            }
+            const index2 = this.testingTasks.indexOf(task);
+            if (index2 !== -1) {
+                this.testingTasks.splice(index2, 1);
+                this.completedTasks.push(task);
+                return; // Вернуться после перемещения в столбец "Выполненные"
+            }
+            const index3 = this.completedTasks.indexOf(task);
+            if (index3 !== -1){
+                this.completedTasks.splice(index3, 1);
+                this.testingTasks.push(task);
+                return;
+            }
+        },
         clearForm() {
             this.newTaskTitle = '';
             this.newTaskDescription = '';
             this.newTaskDeadline = '';
-        },
-        editTask(task) {
-
-        },
-        deleteTask(index) {
-            this.plannedTasks.splice(index, 1);
-        },
-        moveTask(task) {
-
         }
     }
 });
