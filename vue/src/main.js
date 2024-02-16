077dcc0e4d507584d37e0830d41f20223f2f233e
alPumpur <aleftinapumpur@gmail.com>
2024-02-16 22:45:45 +0700

    Удаление и форма

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,3 +1,37 @@
+// main.js
+Vue.component('kanban-column', {
+    props: ['id', 'title', 'tasks', 'allowEdit', 'allowMove'],
+    template: `
+        <div class="column" :id="id">
+            <h2>{{ title }}</h2>
+            <div v-for="(task, index) in tasks" :key="task.created">
+                <div class="task-card">
+                    <h3>{{ task.title }}</h3>
+                    <p>{{ task.description }}</p>
+                    <p>Дата создания: {{ task.created }}</p>
+                    <p>Дедлайн: {{ task.deadline }}</p>
+                    <p v-if="allowEdit">Последнее редактирование: {{ task.lastEdited }}</p>
+                    <button v-if="allowEdit" @click="editTask(task)">Редактировать</button>
+                    <button @click="deleteTask(index)">Удалить</button>
+                    <button v-if="allowMove" @click="moveTask(task)">В следующий столбец</button>
+                </div>
+            </div>
+        </div>
+    `,
+    methods: {
+        editTask(task) {
+            task.lastEdited = new Date().toLocaleString();
+            this.$emit('edit-task', task);
+        },
+        deleteTask(index) {
+            this.$emit('delete-task', index);
+        },
+        moveTask(task) {
+            this.$emit('move-task', task);
+        }
+    }
+});
+
 new Vue({
     el: '#app',
     data: {
@@ -10,36 +44,35 @@ new Vue({
         newTaskDeadline: ''
     },
     methods: {
-        addTask: function(column) {
+        addTask(column) {
             if (this.newTaskTitle.trim() === '') return;
 
             const task = {
                 title: this.newTaskTitle,
                 description: this.newTaskDescription,
                 created: new Date().toLocaleString(),
-                deadline: this.newTaskDeadline
+                deadline: this.newTaskDeadline,
+                lastEdited: new Date().toLocaleString()
             };
 
-            switch (column) {
-                case 'planned':
-                    this.plannedTasks.push(task);
-                    break;
-                case 'inProgress':
-                    this.inProgressTasks.push(task);
-                    break;
-                case 'testing':
-                    this.testingTasks.push(task);
-                    break;
-                case 'completed':
-                    this.completedTasks.push(task);
-                    break;
-                default:
-                    break;
+            if (column === 'planned') {
+                this.plannedTasks.push(task);
             }
-
+            this.clearForm();
+        },
+        clearForm() {
             this.newTaskTitle = '';
             this.newTaskDescription = '';
             this.newTaskDeadline = '';
+        },
+        editTask(task) {
+
+        },
+        deleteTask(index) {
+            this.plannedTasks.splice(index, 1);
+        },
+        moveTask(task) {
+
         }
     }
 });
